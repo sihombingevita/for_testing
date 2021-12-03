bf6ba7c99c6c312de8f6b8c750c1edc90c33c482
evita sihombing <veronikaevita@gmail.com>
2021-12-03 22:09:26 +0700

    first commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Evita Sihombing on 03/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
